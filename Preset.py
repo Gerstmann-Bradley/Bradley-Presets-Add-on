4558a142d562b1af14976cd98ebd3e4a6ae2560a
Gerstmann-Bradley <55994236+Gerstmann-Bradley@users.noreply.github.com>
2024-03-15 15:10:23 +0100

    Update Preset.py

--- Preset.py
+++ Preset.py
@@ -7,28 +7,29 @@ from platform import system
 from subprocess import Popen
 import json
 
-from .constants import BRD_CONST_DATA
-from .Logger import log
-from .utils import connected_to_internet
+from .constants import BRD_CONST_DATA  # Import constants from the add-on
+from .Logger import log  # Import logger from the add-on
+from .utils import connected_to_internet  # Import utility function from the add-on
 
 
+# Define the operator class for updating the add-on
 class BRD_Update(bpy.types.Operator):
     bl_idname = "bradley.update"
     bl_label = "bradley update"
 
     def execute(self, context):
+        # Check if there is an internet connection
         if connected_to_internet():
+            # Send a request to GitHub to get the repository data
             self.r = requests.get(BRD_CONST_DATA.Repository)
-
             self.r = self.r.json()
 
+            # Extract the preset data from the repository
             self.preset_data = [i for i in self.r if i["name"].endswith(".blend")][0]
-
             self.sha = self.preset_data["sha"]
             log.debug(f"sha new -> {self.sha}")
             log.debug(f"sha current -> {BRD_CONST_DATA.__DYN__.sha}")
             self.version = self.preset_data["name"].lower().replace(" ", "")
-
             self.file_repo = self.preset_data["download_url"]
 
             log.debug(f"Preset github version: {self.version}")
@@ -44,29 +45,29 @@ class BRD_Update(bpy.types.Operator):
                 if i.name.endswith(".blend")
             ]
 
+            # Check if the preset needs updating
             if (
                 BRD_CONST_DATA.__DYN__.P_Version == "__"
                 or BRD_CONST_DATA.__DYN__.P_Version != self.version
                 or self.sha != BRD_CONST_DATA.__DYN__.sha
                 or a == []
             ):
-
                 log.debug("Preset -> Updating")
 
+                # Remove previous preset file if necessary
                 if (
                     bool(stuff["__DYN__"]["File_Location"])
                     and not stuff["__DYN__"]["File_Location"] == "."
                     and a != []
                 ):
-
                     BRD_CONST_DATA.File_Location().unlink()
 
+                # Download and save new preset file
                 self.local_filename = PurePath(
                     BRD_CONST_DATA.Folder,
                     BRD_CONST_DATA.__DYN__.B_Version,
                     "preset.blend",
                 )
-                log.debug(str(self.local_filename))
 
                 File_Location = str(self.local_filename)
                 P_Version = self.version
@@ -75,6 +76,7 @@ class BRD_Update(bpy.types.Operator):
                     with open(str(self.local_filename), "wb") as f:
                         shutil.copyfileobj(r.raw, f)
 
+                # Update add-on settings
                 stuff["__DYN__"] = {
                     "New": False,
                     "P_Version": P_Version,
@@ -101,6 +103,7 @@ class BRD_Link(bpy.types.Operator):
     bl_label = "bradley link"
 
     def execute(self, context):
+        # Check if the preset is already linked
         if [
             i
             for i in Path(
@@ -113,6 +116,7 @@ class BRD_Link(bpy.types.Operator):
         self.installed = [i.name for i in bpy.data.node_groups]
         self.por = []
 
+        # Load preset and link node groups
         with bpy.data.libraries.load(
             str(BRD_CONST_DATA.File_Location().resolve()),
             link=True,
@@ -140,6 +144,7 @@ class BRD_Force_Update(bpy.types.Operator):
         return True
 
     def execute(self, context):
+        # Check for internet connection
         if connected_to_internet():
             log.debug("Force Update")
 
@@ -175,7 +180,6 @@ class BRD_Force_Update(bpy.types.Operator):
                     BRD_CONST_DATA.__DYN__.B_Version,
                     "preset.blend",
                 )
-                log.debug(str(self.local_filename))
 
                 File_Location = str(self.local_filename)
                 P_Version = self.version
@@ -235,4 +239,5 @@ class BRD_Folder(bpy.types.Operator):
         return {"FINISHED"}
 
 
+# List of operators provided by the add-on
 preset_help = [BRD_Folder, BRD_Link, BRD_Update, BRD_Force_Update]
